b94907e8427ca48ff6129f663b0b40a2390acdae
yuxy000 <8534353@qq.com>
2018-03-13 15:21:04 +0800

    app 骨架

---
+++ www/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+"""
+---------------------------------
+@Time     : 2018/3/13 15:12
+@Author   : yuxy
+@File     : app.py
+@Project  : awesome-python3-webapp
+----------------------------------
+"""
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body='<h1>Awesome</h1>', content_type='text/html')
+
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
